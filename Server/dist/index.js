7cd40fcabce9185b5a0d39395ebbe6b9a7e2a5f5
notgoodatpickingnames <32577550+notgoodatpickingnames@users.noreply.github.com>
2021-07-14 22:05:09 -0500

    Adding Web App

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -8,7 +8,7 @@ const app = express_1.default();
 const port = 8080; // default port to listen
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.sendFile(__dirname + "/index.html");
 });
 // start the Express server
 app.listen(port, () => {
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -4,11 +4,11 @@ const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get("/", ( req, res ) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 // start the Express server
-app.listen( port, () => {
+app.listen(port, () => {
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
